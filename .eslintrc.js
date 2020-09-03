1222146f57c9d95d5c8d77aa5d93950bfe808999
Jan Hromádka <cumlikator1@email.cz>
2020-09-03 20:48:40 +0200

    eslint cfg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,22 @@
 module.exports = {
-  ...,
-  extends: [
-    'plugin:vue/recommended',
+
+  "root": true,
+  "extends": [
+    "plugin:vue/essential",
+    "eslint:recommended"
   ],
-  rules: {
-    ...
-    // vue rules
-    "vue/order-in-components": ["error", {
-      "order": [
-        "el",
-        "name",
-        "parent",
-        "functional",
-        ["delimiters", "comments"],
-        ["components", "directives", "filters"],
-        "extends",
-        "mixins",
-        "inheritAttrs",
-        "model",
-        ["props", "propsData"],
-        "data",
-        "computed",
-        "watch",
-        "LIFECYCLE_HOOKS",
-        "methods",
-        ["template", "render"],
-        "renderError"
-      ]
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'never'
-    }],
-    'vue/html-closing-bracket-spacing': ['error', {
-      'startTag': 'never',
-      'endTag': 'never',
-      'selfClosingTag': 'always'
-    }],
-    'vue/script-indent': ['error', 2, {
-      'baseIndent': 0,
-      'switchCase': 1,
-      'ignores': []
-    }],
-    'vue/max-attributes-per-line': [2, {
-      'singleline': 1,
-      'multiline': {
-        'max': 1,
-        'allowFirstLine': false
-      }
-    }],
-  },
-  ...
-};
+  "parserOptions": {
+        "ecmaVersion": 9,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "esversion": 9,
+            "destructuring": true,
+            "generators": true,
+            "superInFunctions": true,
+            "experimentalObjectRestSpread": true,
+        }
+    },
+  "rules": {
+    "no-unused-vars": "error",
+  }
+}
