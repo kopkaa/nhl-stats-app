c035cd7abf4e226bc5f7528642715fbec0dd7787
kopkaa <cumlikator1@email.cz>
2021-10-11 10:14:11 +0200

    Todo added getteam

--- client/src/models/Team.js
+++ client/src/models/Team.js
@@ -1,84 +1,89 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 // eslint-disable-next-line import/prefer-default-export
-export const GET_TEAMS = gql` query GetTeams($season: Int!) {
-	getTeams(season: $season) {
-		id
-		name
-		teamName
-		abbreviation
-		locationName
-		officialSiteUrl
-		logoUrl
-	}
-}`;
-
-export const GET_TEAM = gql` query GetTeam($id: Int!, $season: Int!) {
-		getTeam(id: $id, season: $season) {
-			id
-			name
-			division {
-				id
-				name
-			}
-			players {
-				id,
-				fullName,
-				height,
-				weight,
-				positionCode,
-				stats {
-					hits
-					assists
-					shots
-					timeOnIce
-					goals
-					games
-					powerPlayGoals
-					powerPlayPoints
-					powerPlayTimeOnIce
-					shotPct
-					shifts
-					points
-					timeOnIcePerGame
-					plusMinus
-				}
-			}
-			teamName
-			abbreviation
-			locationName
-			officialSiteUrl
-			logoUrl
-		}
-
-}`;
-
-export const GET_ALL_TEAMS_STATS = gql` 
-query GetTeams($season: Int!) {
-	getTeams(season: $season) {
-		id
-		name
-		teamName
-		logoUrl
-		division {
-			id
-			name
-		}
-    stats {
-			gamesPlayed
-			wins
-			losses
-			pts
-			goalsPerGame
-			goalsAgainstPerGame
-			shotsPerGame
-			shotsAllowed
-			powerPlayOpportunities
+export const GET_TEAMS = gql`
+  query GetTeams($season: Int!) {
+    getTeams(season: $season) {
+      id
+      name
+      teamName
+      abbreviation
+      locationName
+      officialSiteUrl
+      logoUrl
     }
-		standing {
-			goalsScored
-			goalsAgainst
-			goalDiff
-		}
-	}
-}`;
+  }
+`;
+
+export const GET_TEAM = gql`
+  query GetTeam($id: Int!, $season: Int!) {
+    getTeam(id: $id, season: $season) {
+      id
+      name
+      division {
+        id
+        name
+      }
+      # TODO pomale players
+      players {
+        id
+        fullName
+        height
+        weight
+        positionCode
+        stats {
+          hits
+          assists
+          shots
+          timeOnIce
+          goals
+          games
+          powerPlayGoals
+          powerPlayPoints
+          powerPlayTimeOnIce
+          shotPct
+          shifts
+          points
+          timeOnIcePerGame
+          plusMinus
+        }
+      }
+      teamName
+      abbreviation
+      locationName
+      officialSiteUrl
+      logoUrl
+    }
+  }
+`;
+
+export const GET_ALL_TEAMS_STATS = gql`
+  query GetTeams($season: Int!) {
+    getTeams(season: $season) {
+      id
+      name
+      teamName
+      logoUrl
+      division {
+        id
+        name
+      }
+      stats {
+        gamesPlayed
+        wins
+        losses
+        pts
+        goalsPerGame
+        goalsAgainstPerGame
+        shotsPerGame
+        shotsAllowed
+        powerPlayOpportunities
+      }
+      standing {
+        goalsScored
+        goalsAgainst
+        goalDiff
+      }
+    }
+  }
+`;
